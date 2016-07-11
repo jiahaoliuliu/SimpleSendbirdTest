0b744aae112fbe1e9e176e0387406f5ec62eb8d6
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-07-11 21:04:34 +0400

    Added the missing layouts

---
+++ app/src/main/java/com/jiahaoliuliu/simplesendbirdtest/MessagesListAdapter.java
@@ -0,0 +1,9 @@
+package com.jiahaoliuliu.simplesendbirdtest;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by jiahaoliuliu on 7/11/16.
+ */
+public class MessagesListAdapter extends RecyclerView.Adapter<ViewHolder> {
+}
